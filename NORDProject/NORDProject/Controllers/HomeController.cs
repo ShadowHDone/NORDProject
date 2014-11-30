7552575a2a9105fd50149bf7f45ef0718d272090
ShadowHD33RUS <ShadowHD33RUS@gmail.com>
2014-12-01 02:48:09 +0300

    Создал сайт (полурабочая версия, прям ваще только основа)

---
+++ NORDProject/NORDProject/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NORDProject.Controllers
+{
+    [HandleError]
+    public class HomeController : Controller
+    {
+        static string nordp = "NORDP | ";
+
+        public ActionResult Index()
+        {
+            ViewData["Message"] = "Все ленты новостей";
+            ViewData["Title"] = nordp + "Все ленты новостей";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewData["Message"] = "Про NORD Project";
+            ViewData["Title"] = nordp + "Про NORD Project";
+
+            return View();
+        }
+
+        public ActionResult ViewPage1()
+        {
+            return View();
+        }
+    }
+}
---
+++ NORDProject/NORDProject/Views/Home/Index.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" MasterPageFile="~/Views/Shared/Site.Master" Inherits="System.Web.Mvc.ViewPage" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="TitleContent" runat="server">
+    <title><%: ViewData["Title"] %></title>
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="PageHeaderContent" runat="server">
+    <nobr><%: ViewData["Message"] %></nobr>
+</asp:Content>
+
+<asp:Content ID="Content3" ContentPlaceHolderID="MainContent" runat="server">
+
+    <p>
+        Тут что-то должно скоро быть.
+    </p>
+</asp:Content>
